1c92dcb034feb8fe3c36b00fea7e3a4f20bbf08a
Naza <uzoukwuc@tcd.ie>
2024-09-03 16:18:20 +0100

    Add argv support

--- evaluation_example.py
+++ evaluation_example.py
@@ -1,22 +1,18 @@
-
-
-from utils import (load_problem_data,
-                   load_solution)
+from utils import (load_problem_data, load_solution)
 from evaluation import evaluation_function
+from argparse import ArgumentParser
 
+parser = ArgumentParser()
+parser.add_argument('file', default='./data/solution_example.json')
+args = parser.parse_args()
 
 # LOAD SOLUTION
-solution = load_solution('./data/solution_example.json')
+solution = load_solution(args.file)
 
 # LOAD PROBLEM DATA
 demand, datacenters, servers, selling_prices = load_problem_data()
 
 # EVALUATE THE SOLUTION
-score = evaluation_function(solution,
-                            demand,
-                            datacenters,
-                            servers,
-                            selling_prices,
-                            seed=123)
+score = evaluation_function(solution, demand, datacenters, servers, selling_prices, seed=122)
 
 print(f'Solution score: {score}')
